ed111517ffedcafeba964b42117e93f85f1f246e
scsaralfred <alfredoscarano87@gmail.com>
2018-07-04 22:52:44 +0800

    Server settings

--- server/server.js
+++ server/server.js
@@ -21,12 +21,12 @@ const port = process.env.PORT || 5000;
 // MIDDLEWARES
 app.use(bodyParser.json());
 app.use(function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Origin, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, X-Response-Time, X-PINGOTHER, X-CSRF-Token, Authorization, x-auth' );
-    res.header('Access-Control-Allow-Methods', '*');
-    res.header('Access-Control-Expose-Headers', 'X-Api-Version, x-auth, X-Request-Id, X-Response-Time');
-    res.header('Access-Control-Max-Age', '1000');
-    res.header("Content-Type", "application/json;charset=utf-8");
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, X-Response-Time, X-PINGOTHER, X-CSRF-Token, Authorization, x-auth' );
+    res.setHeader('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS');
+    res.setHeader('Access-Control-Expose-Headers', 'X-Api-Version, x-auth, X-Request-Id, X-Response-Time');
+    res.setHeader('Access-Control-Max-Age', '1000');
+    res.setHeader("Content-Type", "application/json;charset=utf-8");
     next();
 });
 
