dd03a5351250ad495c51e104efb60e575e36a8b2
scsaralfred <alfredoscarano87@gmail.com>
2018-07-04 23:54:32 +0800

    Setting server

--- server/server.js
+++ server/server.js
@@ -22,11 +22,10 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(function (req, res, next) {
     res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, X-Response-Time, X-PINGOTHER, X-CSRF-Token, Authorization, X-Auth' );
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, X-Response-Time, X-PINGOTHER, X-CSRF-Token, Authorization' );
     res.setHeader('Access-Control-Allow-Methods', '*');
-    res.setHeader('Access-Control-Expose-Headers', 'X-Api-Version, X-Auth, X-Request-Id, X-Response-Time');
+    res.setHeader('Access-Control-Expose-Headers', 'X-Api-Version, x-auth, X-Request-Id, X-Response-Time');
     res.setHeader('Access-Control-Max-Age', '1000');
-    res.setHeader("Content-Type", "application/json;charset=utf-8");
     next();
 });
 
