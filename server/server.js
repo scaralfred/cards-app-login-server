1550b4812baf379fc245612e1956e499678d88cb
scsaralfred <alfredoscarano87@gmail.com>
2018-07-04 23:28:43 +0800

    Setting server

--- server/server.js
+++ server/server.js
@@ -213,21 +213,33 @@ app.post('/users', (req, res) => {
 
 app.post('/users/login', (req, res) => {
    var body = _.pick(req.body, ['email','password']);
-    
-    User.findByCredentials(body.email, body.password)
-    .then((userFound) => user = userFound )
-    .then((user) => School.findOne({ _id: user.schoolID }) )
-    .then((school) => schoolSettings = school.classSettings )
-    .then(() => user.generateAuthToken()
-        .then((token) => {   
-        res.header('x-auth', token).send({user, schoolSettings}); 
-        })
-    )
-    .catch((e) => {
-        res.status(400).send();
+     
+    User.findByCredentials(body.email, body.password).then((user) => {
+       return user.generateAuthToken().then((token) => {   res.header('x-auth', token).send(user);
+        });
+    }).catch((e) => {
+        res.status(400).send(e);
     });
-
 });
+
+
+//app.post('/users/login', (req, res) => {
+//   var body = _.pick(req.body, ['email','password']);
+//    
+//    User.findByCredentials(body.email, body.password)
+//    .then((userFound) => user = userFound )
+//    .then((user) => School.findOne({ _id: user.schoolID }) )
+//    .then((school) => schoolSettings = school.classSettings )
+//    .then(() => user.generateAuthToken()
+//        .then((token) => {   
+//        res.header('x-auth', token).send({user, schoolSettings}); 
+//        })
+//    )
+//    .catch((e) => {
+//        res.status(400).send();
+//    });
+//
+//});
 
 // GET ROUTE
 
