5646bb45beb2405253a76fce9bfc6a1c142b1643
scsaralfred <alfredoscarano87@gmail.com>
2018-07-05 00:14:33 +0800

    Server settings

--- server/server.js
+++ server/server.js
@@ -21,13 +21,13 @@ const port = process.env.PORT || 5000;
 // MIDDLEWARES
 app.use(bodyParser.json());
 app.use(function (req, res, next) {
-    
+    res.setHeader("Cache-Control", "no-cache");
     res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, X-Response-Time, X-PINGOTHER, X-CSRF-Token,Authorization');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, X-Response-Time, X-PINGOTHER, X-CSRF-Token, Authorization, x-auth' );
     res.setHeader('Access-Control-Allow-Methods', '*');
     res.setHeader('Access-Control-Expose-Headers', 'X-Api-Version, x-auth, X-Request-Id, X-Response-Time');
     res.setHeader('Access-Control-Max-Age', '1000');
-    
+    res.setHeader("Content-Type", "application/json;charset=utf-8");
     next();
 });
 
